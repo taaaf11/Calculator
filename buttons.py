b6a45d4b16353563d06646ed2cfe66488ee9fd7a
taaaf11 <taafuuu@gmail.com>
2023-11-25 18:03:58 +0500

    Add separate file for button definitions

---
+++ buttons.py
@@ -0,0 +1,26 @@
+import flet as ft
+
+button_C = ft.ElevatedButton(text='C')
+button_AC = ft.ElevatedButton(text='AC')
+button_empty = ft.ElevatedButton(text=' ')
+button_add = ft.ElevatedButton(text='+')
+button_sub = ft.ElevatedButton(text='-')
+button_mul = ft.ElevatedButton(text='x')
+button_div = ft.ElevatedButton(text='÷')
+button_equals = ft.ElevatedButton(text='=')
+
+button_7 = ft.ElevatedButton(text='7')
+button_8 = ft.ElevatedButton(text='8')
+button_9 = ft.ElevatedButton(text='9')
+
+button_4 = ft.ElevatedButton(text='4')
+button_5 = ft.ElevatedButton(text='5')
+button_6 = ft.ElevatedButton(text='6')
+
+button_1 = ft.ElevatedButton(text='1')
+button_2 = ft.ElevatedButton(text='2')
+button_3 = ft.ElevatedButton(text='3')
+
+button_0 = ft.ElevatedButton(text='0')
+button_point = ft.ElevatedButton(text='.')
+button_empty = ft.ElevatedButton(text=' ')
